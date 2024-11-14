d90f2be1fd1a443daee575ad8b9392a2c3325c29
azulperty <azulperty@gmail.com>
2024-11-14 09:14:18 -0300

    subiendo archivos que se habian dañado de pagina de inicio

--- demo/target/classes/static/js/app.js
+++ demo/target/classes/static/js/app.js
@@ -4,13 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const registerForm = document.querySelector(".contenedor-form-registro");
     const loginLink = document.querySelector(".login-link");
     const registerLink = document.querySelector(".registrar-link");
-    const btn = document.querySelector(".btn");
+    const btn = document.querySelector(".btn-iniciar-sesion");
     const btnRegistrar = document.querySelector(".btn-registrar");
     const iconoCerrar = document.querySelector(".icono-cerrar");
 
 // Mostrar formulario de registro
 registerLink.addEventListener("click", (event) => {
     event.preventDefault();
+    fondo.classList.remove('normal');
     fondo.classList.add('active'); // Mostrar el fondo
     loginForm.style.display = 'none'; // Ocultar login
     registerForm.style.display = 'block'; // Mostrar registro
@@ -19,18 +20,22 @@ registerLink.addEventListener("click", (event) => {
 // Mostrar formulario de inicio de sesión
 loginLink.addEventListener("click", (event) => {
     event.preventDefault();
-    fondo.classList.add('active'); // Mostrar el fondo
+    //fondo.classList.remove('active');  Mostrar el fondo
+    fondo.classList.add('normal');
     registerForm.style.display = 'none'; // Ocultar registro
     loginForm.style.display = 'block'; // Mostrar login
 });
 
-
+//Botón de Iniciar Sesion ARRIBA
 btn.addEventListener("click", (event) => {
     event.preventDefault();
     console.log("Botón de Iniciar Sesión clicado");
-    fondo.classList.remove('active');
+    fondo.classList.add('normal');
+    registerForm.style.display = 'none'; // Ocultar registro
+    loginForm.style.display = 'block'; // Mostrar login
 });
 
+// Botón de registrar grande
 btnRegistrar.addEventListener("click", (event) => {
     event.preventDefault();
     console.log("Botón de Registrar clicado");
@@ -40,6 +45,7 @@ btnRegistrar.addEventListener("click", (event) => {
 // Botón de cerrar
 iconoCerrar.addEventListener("click", () => {
     fondo.classList.remove('active');
+    fondo.classList.remove('normal');
     loginForm.style.display = 'none';  // Ocultar formulario de login
     registerForm.style.display = 'none'; // Ocultar formulario de registro
 });
